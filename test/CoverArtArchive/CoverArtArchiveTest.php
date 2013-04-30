0b12f9563577476ab48e4ec9e56da04e3f3752e4
Mike Almond <mike@res.im>
2013-04-30 10:25:29 -0400

    Create PHPUnit scaffolding for tests

---
+++ test/CoverArtArchive/CoverArtArchiveTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace CoverArtArchive;
+
+/**
+ * @covers CoverArtArchive\
+ */
+class CoverArtArchiveTest extends \PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+    }
+}
