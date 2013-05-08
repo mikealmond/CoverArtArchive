f9dbf578489cd070fc04a2a3b3ac9db6d800992c
Mike Almond <mike@res.im>
2013-05-08 09:11:13 -0400

    Add a unit test to test valid MBID's

---
+++ tests/CoverArtArchive/Tests/CoverArtTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CoverArtArchive\Tests;
+
+use CoverArtArchive\CoverArt;
+
+/**
+ * @covers CoverArtArchive\CoverArt
+ */
+class CoverArtTest extends \PHPUnit_Framework_TestCase
+{
+    public function MBIDProvider()
+    {
+        return array(
+            array(true, '4dbf5678-7a31-406a-abbe-232f8ac2cd63')
+          , array(true, '4dbf5678-7a31-406a-abbe-232f8ac2cd63')
+          , array(false, '4dbf5678-7a314-06aabb-e232f-8ac2cd63') // invalid spacing for UUID's
+          , array(false, '4dbf5678-7a31-406a-abbe-232f8az2cd63') // z is an invalid character
+        );
+    }
+
+    /**
+     * @dataProvider MBIDProvider
+     */
+    public function testIsValidMBID($validation, $mbid)
+    {
+        $this->assertEquals($validation, CoverArt::isValidMBID($mbid));
+    }
+}
